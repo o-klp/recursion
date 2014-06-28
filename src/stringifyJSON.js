49a077e2b5bad2f373a1931498d25ca4eb65b7f9
Keenan Lidral-Porter <klp2012@gmail.com>
2014-06-28 17:46:46 -0400

    started on stringifyJSON(obj)-checks type of obj and adds it to a string diff qualifiers (, " [ { etc.) depending on type currently passes first 6 tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,35 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+	var string = '';
+	var type = typeof(obj);
+
+	if(type === 'object'){	//obj is an object
+		if(obj === null){	//obj is null object
+			string += obj;
+		} else if(Array.isArray(obj)){	//obj is an array
+			string += '[';
+			if(obj.length){
+				for(var i = 0; i < obj.length-1; i++){
+					stringifyJSON(obj[i]);
+					string += ',';
+				}
+				stringifyJSON(obj[obj.length-1]);
+			}
+			string += ']';
+		} else {	//obj is {} object
+			string += '{';
+			for(var x in obj){
+				stringifyJSON(x);
+				string += ':';
+				stringifyJSON(obj[x]);
+			}
+			string += '}';
+		}
+	} else if(type === 'string'){	//obj is string
+		string += '"' + obj + '"';
+	} else {	//obj is none of above (bool, num)
+		string += obj;
+	}
+	return string;
 };
