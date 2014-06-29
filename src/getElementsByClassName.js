4c4aa0f563a521f56c1b60bcc45d6517ad10f0bf
Keenan Lidral-Porter <klp2012@gmail.com>
2014-06-29 14:38:51 -0400

    added getElementsByClassName-contains a function which tests if className is in object calls same test on all object[element].childNodes getElementsByClassName passes all tests

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,4 +6,21 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
   // your code here
+  var result =[];
+  var holder = document.childNodes;
+  var recurse = function(object) {
+	for (var i = 0; i < object.length; i++) {
+		if(object[i].classList){
+			if(object[i].classList.contains(className)){
+				console.log(object[i]);
+				result.push(object[i]);
+			}
+		}
+		if(object[i].hasChildNodes()){
+			recurse(object[i].childNodes);
+		}
+	}
+	};
+  recurse(holder);
+  return result;
 };
